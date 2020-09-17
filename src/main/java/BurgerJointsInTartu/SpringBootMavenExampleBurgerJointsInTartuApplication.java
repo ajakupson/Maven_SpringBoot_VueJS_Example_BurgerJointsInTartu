cd5a7302e25d3fa60e75bfc2e7adaae6fc993182
Andrei Jakupson <ajakupson.job@gmail.com>
2020-09-17 19:29:00 +0300

    deploy

--- src/main/java/BurgerJointsInTartu/SpringBootMavenExampleBurgerJointsInTartuApplication.java
+++ src/main/java/BurgerJointsInTartu/SpringBootMavenExampleBurgerJointsInTartuApplication.java
@@ -2,20 +2,10 @@ package BurgerJointsInTartu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @SpringBootApplication
 public class SpringBootMavenExampleBurgerJointsInTartuApplication {
 
-	// for heroku deployment
-	@GetMapping("/*")
-	public String index() {
-		return "index";
-	}
-    
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMavenExampleBurgerJointsInTartuApplication.class, args);
 	}
