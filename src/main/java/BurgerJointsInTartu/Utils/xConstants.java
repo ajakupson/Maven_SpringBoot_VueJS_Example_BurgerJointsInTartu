7264bee32fa0c633b2326069de4b7860b85099a2
Andrei Jakupson <ajakupson.job@gmail.com>
2020-09-18 15:16:58 +0300

    deploy

--- src/main/java/BurgerJointsInTartu/Utils/xConstants.java
+++ src/main/java/BurgerJointsInTartu/Utils/xConstants.java
@@ -17,8 +17,8 @@ public class xConstants {
 	public static final String FOURSQUARE_CLIENT_ID = "ULTN0IESIICWPHWBN1RHEVGCK3HXNN1TD2MQ1PT2M0AZVNQL";
 	public static final String FOURSQUARE_CLIENT_SECRET = "ZL0BMF3ZAUAUAEM5W3SQI5FOLSVUSMYL0QE11SVFNRCJHACC";
 	
-	public static final String AWS_IAM_ACCESS_KEY = "AKIAJ2VOITPUSU4R45TA";
-	public static final String AWS_IAM_SECRET_ACCESS_KEY = "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";
+	public static final String AWS_IAM_ACCESS_KEY = "{AWS_IAM_ACCESS_KEY}";
+	public static final String AWS_IAM_SECRET_ACCESS_KEY = "{AWS_IAM_SECRET_ACCESS_KEY}";
 	public static final String AWS_REGION = "eu-west-1";
 	public static final String AWS_API_GATEWAY_ENPOINT = "https://pplkdijj76.execute-api.eu-west-1.amazonaws.com/prod";
 	
